4a60ff69b5ebddfce4936df70e1b77836602d8ec
xinyuwang <xinyucs@gmail.com>
2018-07-05 18:21:39 +0800

    Init project

---
+++ src/file.js
@@ -0,0 +1,35 @@
+﻿const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+
+class File {
+
+    constructor(filepath) {
+
+        this.filepath = path.resolve(filepath);
+
+    }
+
+    collect(cfg) {
+
+        const { glob = "*", regex = ".*", recursive = true, digest = "sha256", size = [-1, -1] } = cfg;
+
+
+
+    }
+
+
+
+    static diff(src, dst) {
+
+
+        
+    }
+
+
+    _p(msg) {
+
+    }
+
+
+};
---
+++ test.js
@@ -0,0 +1,3 @@
+﻿const fd = require(./filediff);
+
+
