3a1aec530125316bb155f5c362fb6841fb1e7ae5
xinyuwang <xinyucs@gmail.com>
2018-07-06 22:32:00 +0800

    filediff

--- src/filediff.js
+++ src/filediff.js
@@ -6,12 +6,11 @@ import File from "./file";
 export default class FileDiff {
 
     constructor(f1, f2) {
-
+        this.f1 = f1;
+        this.f2 = f2;
     }
 
-
-
-    collect(cfg) {
+    collect({ }) {
 
         const { glob = '*', regex = '.*', recursive = true, digest = 'sha256', size = [-1, -1] } = cfg;
 
